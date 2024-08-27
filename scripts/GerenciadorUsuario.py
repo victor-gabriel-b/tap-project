2c77aa081b5f390623ae2f4ed67439bd4cea328e
victor-gabriel-b <victor.gabrieljp8@gmail.com>
2024-08-26 23:37:49 -0300

    Adicionar persistência via arquivo binário

--- scripts/GerenciadorUsuario.py
+++ scripts/GerenciadorUsuario.py
@@ -1,17 +1,29 @@
+import pickle
+import os
 from Database import *
 from Usuario import *
-from scripts.login_validation import *
-
+from login_validation import *
+CAMINHO_DATABASE = "dados_usuario.pkl"
 
 class GerenciadorUsuario:
     def __init__(self):
         self.criar_conexao()
 
     def criar_conexao(self):
+        # Verificar se existe antes de mesmo de tentar com 'try' para que seja possível diferenciar "arquivo não encontrado" de "TÁ TUDO ERRADO NO ARQUIVO"
+        if os.path.isfile(CAMINHO_DATABASE):
+            try:
+                with open(CAMINHO_DATABASE, 'rb') as db_in:
+                    # Caso dê certo, vazar na hora pra não criar de novo lá em baixo
+                    self.database = pickle.load(db_in)
+                    return
+            except IOError as e:
+                print(f"Arquivo existe mas houve falha na leitura. Um banco vazio será criado e o arquivo será sobrescrito ao salvar. Erro: {e}")
+
+        # Qualquer coisa deu levemente errado chega aqui
         self.database = Database()
 
-    def adicionar_usuario(self, username, email, senha):
-        
+    def adicionar_usuario(self, username, email, senha):  
         try:
             validar_username(username)
             validar_senha(senha)
@@ -28,3 +40,13 @@ class GerenciadorUsuario:
     def exibir_usuarios(self):
         for i in range(self.database.getLenght()):
             self.database.read(i)
+
+    def salvar(self):
+        try:
+            with open(CAMINHO_DATABASE, "wb") as db_out:
+                pickle.dump(self.database, db_out, pickle.HIGHEST_PROTOCOL)
+
+        except IOError as e:
+            print(f"Falha ao escrever database como arquivo! Erro: {e}")
+
+

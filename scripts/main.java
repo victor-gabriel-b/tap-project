07ef60676c25e191fc1f5e6112857bb304b0efe7
Alysson-Silva <alyssonlogar123@gmail.com>
2024-10-26 22:44:58 -0300

    Builder incomplete

--- scripts/main.java
+++ scripts/main.java
@@ -1,6 +1,8 @@
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 
+import builder.ConcreteUserBuilder;
+import builder.UserDirector;
 import entity.Login;
 import entity.Music;
 import entity.Score;
@@ -20,12 +22,15 @@ public class main {
         manager.interfaces.ScoreManager scores = dbf.getScoreManager();
         manager.interfaces.MusicManager musics = dbf.getMusicManager();
         manager.interfaces.LoginManager logins = dbf.getLoginManager();
-    
-        var userA = new entity.User(0, "WesleySilva", "wesley.abc@hotmail.com", "Abcdefg1!");
+
+        // Usa o padrao builder para criar objetos do tipo User
+        UserDirector userDirector = new UserDirector(new ConcreteUserBuilder());
+
+        User userA = userDirector.constructUser(0, "WesleySilva", "wesley.abc@hotmail.com", "Abcdefg1!");
         int id = users.add_user(userA);
         userA.setId(id);
 
-        User userB = new entity.User(0, "WisePedrosa", "wise@it.a.coisa.com", "PAoFR@ANC3S");
+        User userB = userDirector.constructUser(0, "WisePedrosa", "wise@it.a.coisa.com", "PAoFR@ANC3S");
         id = users.add_user(userB);
         userB.setId(id);
 
@@ -49,45 +54,24 @@ public class main {
 
         //Music musicB = new Music(0, "The Last of Us", "youtube.com/mmYdf0yqK_Fc", "The Last of Us", "Survival Horror", 2013);
 
-        /* 
-        for(int i = 1; i <2; i ++){
+        
+        for (int i = 1; i < 2; i++) {
             User user = users.read_user(i);
-            System.out.println(i + " " + user.getUsername() + " " + user.getEmail()+ " " + user.getPassword());
-        }
-
-        userA.setUsername("Spines");
-        users.update_user(userA);
-
-        for(int i = 1; i <2; i ++){
-            User user = users.read_user(i);
-            System.out.println(i + " " + user.getUsername() + " " + user.getEmail()+ " " + user.getPassword());
+            System.out.println(i + " " + user.getUsername() + " " + user.getEmail() + " " + user.getPassword());
         }
 
         users.delete(userA);
 
-        for(int i = 1; i <2; i ++){
+        for (int i = 1; i < 2; i++) {
             User user = users.read_user(i);
-            System.out.println(i + " " + user.getUsername() + " " + user.getEmail()+ " " + user.getPassword());
-        }
-        */
-/* 
-        for(int i = 1; i <2; i ++){
-            Score score = scores.read_score(i);
-            System.out.println(i + " " + score.getUserId() + " " + score.getScore());
+            System.out.println(i + " " + user.getUsername() + " " + user.getEmail() + " " + user.getPassword());
         }
 
-        for(int i = 1; i <2; i ++){
-            Music music = musics.read_music(i);
-            System.out.println(i + " " + music.getName() + " " + music.getLink() + " " + music.getGameName() + " " + music.getGenre() + " " + music.getLaunchYear());
-        }
-*/
         ArrayList<Score> scores_user = dbf.getScoresByUsername("WisePedrosa");
-        for(int i = 0; i < scores_user.size(); i++){
-            
+        for (int i = 0; i < scores_user.size(); i++) {
             Score score = scores_user.get(i);
             System.out.println(i + " " + score.getUserId() + " " + score.getScore());
         }
-
 
         ZonedDateTime dataHora;
         var loginA = new Login(0, 1, dataHora = ZonedDateTime.now());
